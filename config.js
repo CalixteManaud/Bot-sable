ca89f880e7c8d5c890a41af1a3f28fa5e2d0f6d5
CalixteManaud <manaud.calixte@etu.univ-amu.fr>
2023-06-13 16:01:53 +0200

    Revoir

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
 module.exports = {
+    IDServeur: '1054353567206604850',
+    IDChannel: '1054353568188080220',
     token : 'MTExNTU2Mjk2OTg0MjA2MTM2NA.Gpbb7v.abVoNsgy7DuMsG4-xdit_BX64xRXyRwX08yda4',
     apiUser: '122218943',
     apiSecret: '7hX32MyuZsqBC3AP9WBQ',
-    work: 'wfl_edZJCSSIo1nRmhugC1P76'
+    work: 'wfl_ee1GlKYsAC3bRRNI4P4nR'
 }
