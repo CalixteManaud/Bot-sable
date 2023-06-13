8b71b24ab4f2c2bc4c1901f55fe7f070a73789e0
Calixte manaud <80592553+CalixteManaud@users.noreply.github.com>
2023-06-13 22:28:38 +0200

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    IDServeur: '1117919355519385670',
-    IDChannel: '1117938915731570728',
-    token : 'MTExNTU2Mjk2OTg0MjA2MTM2NA.Gpbb7v.abVoNsgy7DuMsG4-xdit_BX64xRXyRwX08yda4',
-    apiUser: '122218943',
-    apiSecret: '7hX32MyuZsqBC3AP9WBQ',
-    work: 'wfl_edZJCSSIo1nRmhugC1P76'
-}
